caa920dd0a1b414abf7b53259f8ad3096335dbba
nico jedlinskki <nicolasjedlinski@outlook.com>
2023-07-27 20:10:44 -0300

    ultimas pruebas

---
+++ js/main.js
@@ -0,0 +1,16 @@
+let edad= 23
+edad=30
+
+let nombre= "nicolas"
+
+let usuario= prompt ("Welcome") 
+
+alert ("Bienvenido" + "  " + usuario)
+
+edad=prompt("que edad tienes ?")
+
+alert ("eres mayor de 18 ?")
+
+edad=prompt("si o no ?")
+
+alert ("Gracias Por tu respuesta")
